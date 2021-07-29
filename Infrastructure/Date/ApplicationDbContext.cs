4812986808ad733b50dee54014ace6aac3e9fb0f
Ala Nowak <a.nowak@gmail.com>
2021-07-29 09:19:28 -0400

    Product ProductType ProductBrand

--- Infrastructure/Date/ApplicationDbContext.cs
+++ Infrastructure/Date/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using Core.Entities;
 using Core.Entities.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -11,6 +12,11 @@ public class ApplicationDbContext : IdentityDbContext<AppUser, AppRole, string, 
             : base(options)
         {
         }
+
+        public DbSet<ProductBrand> ProductBrands {get; set;}
+        public DbSet<ProductType> ProductTypes {get; set; }
+
+        public DbSet<Product> Producs {get; set;}
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
